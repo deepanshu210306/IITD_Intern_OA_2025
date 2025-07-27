73f86ce97941b003cb13f0ed993cb2050b5e8ab6
deepanshu210306 <deepanshu210306@gmail.com>
2025-07-27 20:51:15 +0530

    ms

--- microsoft_test.cpp
+++ microsoft_test.cpp
@@ -8,3 +8,5 @@
 // there were many fucntions like inserting , deletion , and we has a cursor to maintain 
 // that points to a certain index and we had to make few function to control movement of that cursor 
 // all the description was given in the question on that basis we have to design the different functions
+
+//OA was really Easy it was trying to judge on the basis of speed
