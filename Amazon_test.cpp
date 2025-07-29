726a8d51e9afd57b4fdb309f0413eb7f709f88b1
deepanshu210306 <deepanshu210306@gmail.com>
2025-07-29 22:25:14 +0530

    amazon OA

---
+++ Amazon_test.cpp
@@ -0,0 +1,10 @@
+// There were 2 question 
+
+// first que tha ki ek array hai usko hume non decreasing banana hai
+
+// second was we have given a power array in which powers of different aarays are stored or hume x server banane hai with x backups 
+// total 2x servers assign karne hai us power arraye me se and the backup server's power should be greater than or equal to main server
+// then finally we were required to find the toal max capacity of the main server (return the total of power of all main servers )
+
+
+// after these two coding questions there was a HR round
